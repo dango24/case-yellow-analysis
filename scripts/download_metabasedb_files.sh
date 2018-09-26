32ed1c61389a1ac6eb0330f4558316ff7856a10c
dangolan <dangolan@amazon.com>
2018-09-26 18:39:08 +0300

    Download metabase binaries to analysis service instance

--- scripts/download_metabasedb_files.sh
+++ scripts/download_metabasedb_files.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/case-yellow/dashboard
-aws s3 sync . s3://cy-codebuild/cy-metabase-artifact/cy-metabase-db.zip
+
+aws s3 cp s3://cy-codebuild/cy-metabase-artifact/cy-metabase-db.zip cy-metabase-db.zip
+unzip cy-metabase-db.zip
+rm -rf cy-metabase-db.zip
