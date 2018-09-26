1c6f27ece0e92a678fa16dce321d43c8d154148d
dangolan <dangolan@amazon.com>
2018-09-26 19:01:13 +0300

    Run metabase process

--- scripts/start_app_process.sh
+++ scripts/start_app_process.sh
@@ -2,3 +2,7 @@
 
 cd /home/ec2-user/case-yellow/
 java -jar case-yellow-analysis.jar > /dev/null 2> /dev/null < /dev/null &
+
+/home/ec2-user/case-yellow/dashboard
+
+/run_dashboard.sh > /dev/null 2> /dev/null < /dev/null &
