6b49888c3ed0ef691a1c8ccaffab8c2d8ce74eac
dangolan <dangolan@amazon.com>
2018-09-27 17:50:34 +0300

    fix redirect to dashboard directory command

--- scripts/start_app_process.sh
+++ scripts/start_app_process.sh
@@ -3,6 +3,6 @@
 cd /home/ec2-user/case-yellow/
 java -jar case-yellow-analysis.jar > /dev/null 2> /dev/null < /dev/null &
 
-/home/ec2-user/case-yellow/dashboard
+cd /home/ec2-user/case-yellow/dashboard
 
-./run_dashboard.sh > /dev/null 2> /dev/null < /dev/null &
+/run_dashboard.sh > /dev/null 2> /dev/null < /dev/null &
