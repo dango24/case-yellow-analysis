0a1bd6bb96c9d88c24c43a36f77218e2bc4c2e40
Dan <dangolan24@gmail.com>
2018-10-01 19:17:51 +0300

    Invoke dashboard process from CodeDeploy script

--- scripts/start_app_process.sh
+++ scripts/start_app_process.sh
@@ -5,4 +5,7 @@ java -jar case-yellow-analysis.jar > /dev/null 2> /dev/null < /dev/null &
 
 cd /home/ec2-user/case-yellow/dashboard
 
-/run_dashboard.sh 7777 > /dev/null 2> /dev/null < /dev/null &
+PORT=7777
+export MB_JETTY_PORT=$PORT
+echo running Metabase on port $PORT
+java -jar metabase.jar > logfile.log 2> logfile.log < logfile.log &
